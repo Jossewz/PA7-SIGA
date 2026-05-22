1db7d9cdaad79a88f92eeb6feb4c43416e276957
Jossewz <joseayolaar@gmail.com>
2026-05-21 22:15:22 -0500

    .

---
+++ src/main/java/com/siga/siga_iea/SigaIeaApplication.java
@@ -0,0 +1,13 @@
+package com.siga.siga_iea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaIeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaIeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siga/siga_iea/SigaIeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siga.siga_iea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaIeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

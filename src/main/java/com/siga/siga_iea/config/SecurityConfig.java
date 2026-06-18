f56264ecb2159e4a9a4f958df6d1ddc951dc1e1d
Jossewz <joseayolaar@gmail.com>
2026-06-18 00:20:00 -0500

    layout inicial

--- src/main/java/com/siga/siga_iea/config/SecurityConfig.java
+++ src/main/java/com/siga/siga_iea/config/SecurityConfig.java
@@ -1,4 +1,36 @@
 package com.siga.siga_iea.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/",
+                                "/css/**",
+                                "/js/**",
+                                "/img/**"
+                        )
+                        .permitAll()
+
+                        .anyRequest()
+                        .authenticated()
+                )
+
+                .formLogin(form -> form.disable());
+
+        return http.build();
+    }
 }
